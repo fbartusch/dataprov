9e6bc64fffd89243fc0a37918fd85e59e02cc525
fbartusch <felix.bartusch@uni-tuebingen.de>
2018-02-09 14:11:29 +0100

    Annotation of several output files now possible

--- README.md
+++ README.md
@@ -39,8 +39,24 @@ First, get bwa from the  biocontainer project.
 docker pull biocontainers/bwa
 ```
 
-Run the biocontainer. The command is wrapped by dataprov. 
+Get the reference genome. We will use data from a snakemake tutorial for this purpose. The command downloads the archive, untars it into the data directory and annotates the genome.fa file. Usually the wrapped command needs no quotes, but here they are needed because of the '&&'.
 
 ```
-dataprov docker run -v $PWD/data:/tmp:z -it docker.io/biocontainers/bwa:latest bwa index /tmp/index/genome.fa
+dataprov -d -o data/genome.fa -m "Download of snakemake tutorial test data" run 'wget https://bitbucket.org/snakemake/snakemake-tutorial/get/v3.11.0.tar.bz2 && mkdir data && tar xvjf  v3.11.0.tar.bz2 -C data --strip-components 2'
 ```
+
+Run bwa in the biocontainer. The command is wrapped by dataprov and genome.fa.bwt is annotated with provenance metadata. 
+
+```
+dataprov -d -o data/genome.fa.bwt -m "Test dataprov" run docker run -v $PWD/data:/tmp:z -it docker.io/biocontainers/bwa:latest bwa index /tmp/genome.fa
+```
+
+If you want to annotate all files created by bwa.
+
+```
+cd data/index
+dataprov -d -o genome.fa.amb genome.fa.ann genome.fa.bwt genome.fa.pac genome.fa.sa -m "Test dataprov" run docker run -v $PWD:/tmp/:z -it docker.io/biocontainers/bwa:latest bwa index /tmp/genome.fa
+```
+
+The commands tend to be very long and unreadable if you want to annotate more result files.
+TODO: Option to annotate all files in one directory (except the input files)
--- dataprov/dataprov.py
+++ dataprov/dataprov.py
@@ -10,6 +10,7 @@ import datetime
 import socket
 from xml.etree import ElementTree
 from xml.dom import minidom
+from collections import defaultdict
 
 
 def prettify(elem):
@@ -85,10 +86,10 @@ def main():
     # If the user uses a workflow manager / workflow engine supported by this tool,
     # The output will be detected automatically
     cwd = os.getcwd()
-    command_output_file = os.path.join(cwd, "dataprov_output")
-    parser.add_argument('-o', '--output',
-                        help="path to the output file of the wrapped command. The provenance file with the same name + .prov suffix will be created beside the output file",
-                        default=command_output_file)
+    command_output_files = [os.path.join(cwd, "dataprov_output")]
+    parser.add_argument('-o', '--output', nargs='+',
+                        help="path to output files of the wrapped command. The provenance files with the same name + .prov suffix will be created beside the output files",
+                        default=command_output_files)
                         
     # If the used command line tool used one or more input files, the user
     # Can specifiy the paths to the files.
@@ -124,15 +125,15 @@ def main():
     debug = args.debug
     executor_config_file = args.executor
     message = args.message
-    command_output_file = args.output
-    input_metadata_files = args.input
+    command_output_files = args.output
+    command_input_files = args.input
     
     if debug:
         print("Arguments: " + str(args))
         print("Remaining: " + str(remaining))
         print("Personal information: " + executor_config_file)
-        print("Command output: " + str(command_output_file))
-        print("Input metadata files: " + str(input_metadata_files))
+        print("Command outputs: " + str(command_output_files))
+        print("Input metadata files: " + str(command_input_files))
         print("Message: ", message)
 
     # Check if executor information are available
@@ -155,92 +156,152 @@ def main():
         print("\tMail: " + executor_config.get('executor', 'mail'))
         print("\tAffiliation: " + executor_config.get('executor', 'affiliation'))
 
-    #TODO
     # Read input provenance metadata
-
+    for input_file in command_input_files:
+        # Check if input file and the corresponding provenance metadata exists
+        if not os.path.exists(input_file):
+            print("Input file specified by -i does not exist: ", input_file)
+            print("No provenance information will be considered for this file.")
+        input_prov_file = input_file + '.prov'
+        if not os.path.exists(input_prov_file):
+            print("Metadate for input file specified by -i does not exist: ", input_prov_file)
+            print("No provenance information will be considered for this file.")
+    
     # Record start datetime
     # YYYY-MM-DDThh:mm:ss
     start_time = datetime.datetime.now().strftime("%Y-%m-%dT%H:%m:%S")
     
     # Execute the wrapped command
-    return_code = subprocess.call(remaining)
+    command_string = ' '.join(remaining)
+    commands = command_string.split('&&')
+    for command in commands:
+        command_list = command.split()
+        return_code = subprocess.call(command_list)
+        continue
 
     # Record end datetime
     # YYYY-MM-DDThh:mm:ss
     end_time = datetime.datetime.now().strftime("%Y-%m-%dT%H:%m:%S")
 
-    # Create resulting provenance data
-    xml_dataprov = Element('dataprov')
-    # file element
-    xml_file = SubElement(xml_dataprov, 'file')
-    xml_file.text = command_output_file
-    
-    # sha1 hashsum of file
-    sha1 = hashlib.sha1()    
-    # BUF_SIZE is totally arbitrary, change for your app!
-    BUF_SIZE = 65536  # lets read stuff in 64kb chunks!
-    if os.path.isfile(command_output_file):
-        with open(command_output_file, 'rb') as f:
-            while True:
-                data = f.read(BUF_SIZE)
-                if not data:
-                    break
-                sha1.update(data)
-        sha1sum = sha1.hexdigest()
-    else:
-        sha1sum = "undefined"
-    xml_sha1 = SubElement(xml_dataprov, 'sha1')
-    xml_sha1.text = sha1sum
-    
-    # The history
-    xml_history = SubElement(xml_dataprov, 'history')
-    
-    # Insert the operations taken from the input files metadata
-    #TODO 
-    
-    # The NEW operation we just executed
-    xml_new_op = SubElement(xml_history, 'op')
-    # StartTime    
-    xml_startTime = SubElement(xml_new_op, 'startTime')
-    xml_startTime.text = str(start_time)
-    # EndTime
-    xml_endTime = SubElement(xml_new_op, 'endTime')
-    xml_endTime.text = str(end_time)
-    # Executor
-    xml_executor = SubElement(xml_new_op, 'executor')
-    xml_executor_honorific = SubElement(xml_executor, 'honorific')
-    xml_executor_honorific.text = executor_config.get('executor', 'honorific')
-    xml_executor_firstName = SubElement(xml_executor, 'firstName')
-    xml_executor_firstName.text = executor_config.get('executor', 'firstName')
-    xml_executor_middleName = SubElement(xml_executor, 'middleName')
-    xml_executor_middleName.text = executor_config.get('executor', 'middleName')
-    xml_executor_surname = SubElement(xml_executor, 'surname')
-    xml_executor_surname.text = executor_config.get('executor', 'surname')
-    xml_executor_suffix = SubElement(xml_executor, 'suffix')
-    xml_executor_suffix.text = executor_config.get('executor', 'suffix')
-    xml_executor_mail = SubElement(xml_executor, 'mail')
-    xml_executor_mail.text = executor_config.get('executor', 'mail')
-    xml_executor_affiliation = SubElement(xml_executor, 'affiliation')
-    xml_executor_affiliation.text = executor_config.get('executor', 'affiliation')
-    # Hostname
-    hostname = socket.gethostname()
-    xml_hostname = SubElement(xml_new_op, 'hostname')
-    xml_hostname.text = hostname
-    # Operation class
-    xml_opClass = SubElement(xml_new_op, 'opClass')
-    xml_opClass.text = 'CommandLine'
-    # WrappedCommand
-    xml_wrappedCommand = SubElement(xml_new_op, 'wrappedCommand')
-    xml_wrappedCommand.text = ' '.join(remaining)
-    # Message
-    xml_message = SubElement(xml_new_op, 'message')
-    xml_message.text = message
-    
-    # Save the provenance xml-file
-    dataprov_file = command_output_file + ".prov"
-    pretty_xml = prettify(xml_dataprov)
-    with open(dataprov_file, "w") as dataprov_file_handler:
-        dataprov_file_handler.write(pretty_xml)
+    # Create an provenance object for each output file
+    # Dictionary: File -> Provenance object (e.g. a dictionary)
+    prov_dicts = defaultdict()
+    for output_file in command_output_files:
+        new_dict = defaultdict()
+        
+        # Because the metadata file is in the same directory as the file, we don't need the path
+        new_file = os.path.basename(output_file)
+        new_dict['file'] = new_file       
+        
+        # sha1 hashsum of file
+        sha1 = hashlib.sha1()    
+        # BUF_SIZE is totally arbitrary, change for your app!
+        BUF_SIZE = 65536  # lets read stuff in 64kb chunks!
+        if os.path.isfile(output_file):
+            with open(output_file, 'rb') as f:
+                while True:
+                    data = f.read(BUF_SIZE)
+                    if not data:
+                        break
+                    sha1.update(data)
+                sha1sum = sha1.hexdigest()
+        else:
+            sha1sum = "undefined"
+        new_dict['sha1'] = sha1sum
+        
+        # Insert the operations taken from the input files metadata
+        #TODO         
+        
+        # History of the file. This is a list, because there can be several operations
+        new_history = defaultdict(list)
+        # The new operation
+        new_op = defaultdict()
+        new_op['startTime'] = start_time
+        new_op['endTime'] = end_time
+        # Executor
+        new_executor = defaultdict()
+        new_executor['honorific'] = executor_config.get('executor', 'honorific')
+        new_executor['firstName'] = executor_config.get('executor', 'firstName')
+        new_executor['middleName'] = executor_config.get('executor', 'middleName')
+        new_executor['surname'] = executor_config.get('executor', 'surname')
+        new_executor['suffix'] = executor_config.get('executor', 'suffix')
+        new_executor['mail'] = executor_config.get('executor', 'mail')
+        new_executor['affiliation'] = executor_config.get('executor', 'affiliation')
+        new_op['executor'] = new_executor
+        # hostname
+        hostname = socket.gethostname()
+        new_op['hostname'] = hostname
+        # Operation class
+        new_op['opClass'] = 'CommandLine'
+        # WrappedCommand
+        new_op['wrappedCommand'] = ' '.join(remaining)
+        # Message
+        new_op['message'] = message
+        # Append the new operation to the history
+        new_history['op'].append(new_op)
+        new_dict['history'] = new_history
+        
+        # Append the new dict to the other provenance dicts
+        prov_dicts[output_file] = new_dict
+        
+    # Create XML-File from each provenance dictionary
+    for file_name, prov_dict in prov_dicts.items():   
+        # Create resulting provenance data
+        xml_dataprov = Element('dataprov')
+        # file element
+        xml_file = SubElement(xml_dataprov, 'file')
+        xml_file.text = file_name
+        
+        xml_sha1 = SubElement(xml_dataprov, 'sha1')
+        xml_sha1.text = prov_dict['sha1']
+        
+        # The history
+        xml_history = SubElement(xml_dataprov, 'history')
+        
+        # Iterate over the list of operations
+        for op in prov_dict['history']['op']:
+            # The NEW operation we just executed
+            xml_new_op = SubElement(xml_history, 'op')
+            # StartTime    
+            xml_startTime = SubElement(xml_new_op, 'startTime')
+            xml_startTime.text = op['startTime']
+            # EndTime
+            xml_endTime = SubElement(xml_new_op, 'endTime')
+            xml_endTime.text = op['endTime']
+            # Executor
+            xml_executor = SubElement(xml_new_op, 'executor')
+            xml_executor_honorific = SubElement(xml_executor, 'honorific')
+            xml_executor_honorific.text = op['executor']['honorific']
+            xml_executor_firstName = SubElement(xml_executor, 'firstName')
+            xml_executor_firstName.text = op['executor']['firstName']
+            xml_executor_middleName = SubElement(xml_executor, 'middleName')
+            xml_executor_middleName.text = op['executor']['middleName']
+            xml_executor_surname = SubElement(xml_executor, 'surname')
+            xml_executor_surname.text = op['executor']['surname']
+            xml_executor_suffix = SubElement(xml_executor, 'suffix')
+            xml_executor_suffix.text = op['executor']['suffix']
+            xml_executor_mail = SubElement(xml_executor, 'mail')
+            xml_executor_mail.text = op['executor']['mail']
+            xml_executor_affiliation = SubElement(xml_executor, 'affiliation')
+            xml_executor_affiliation.text = op['executor']['affiliation']
+            # Hostname
+            xml_hostname = SubElement(xml_new_op, 'hostname')
+            xml_hostname.text = op['hostname']
+            # Operation class
+            xml_opClass = SubElement(xml_new_op, 'opClass')
+            xml_opClass.text = op['opClass']
+            # WrappedCommand
+            xml_wrappedCommand = SubElement(xml_new_op, 'wrappedCommand')
+            xml_wrappedCommand.text = op['wrappedCommand']
+            # Message
+            xml_message = SubElement(xml_new_op, 'message')
+            xml_message.text = op['message']
+        
+            # Save the provenance xml-files
+            dataprov_file = file_name + ".prov"
+            pretty_xml = prettify(xml_dataprov)
+            with open(dataprov_file, "w") as dataprov_file_handler:
+                dataprov_file_handler.write(pretty_xml)
         
     exit(0)
     # CWL
