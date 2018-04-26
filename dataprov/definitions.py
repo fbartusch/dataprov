11bda11e9899f1e6fd9d297fd12c029c37317f66
fbartusch <felix.bartusch@uni-tuebingen.de>
2018-04-26 10:50:21 +0200

    Added XML Namespace variables that can be used by scripts

--- dataprov/definitions.py
+++ dataprov/definitions.py
@@ -5,3 +5,9 @@ ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # Directory with xml-schema files
 XML_DIR = os.path.join(ROOT_DIR, 'xml')
+
+# For the Dataprov XML Namexpace
+DATAPROV_NAMESPACE = "Dataprov"
+DATAPROV = "{%s}" % DATAPROV_NAMESPACE
+NSMAP = {None : DATAPROV_NAMESPACE} # the default namespace (no prefix)
+
